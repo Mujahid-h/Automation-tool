87351d74558e90fde0bbef11969797a7b3d7de74
Mujahid Hussain <hmujahid08@gmail.com>
2024-11-14 00:14:47 +0500

    "Updated console log message in connectDB function"

--- server/config/db.js
+++ server/config/db.js
@@ -7,7 +7,7 @@ dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB 🏪🏪🏪");
   } catch (error) {
     console.error("MongoDB connection error:", error);
     process.exit(1);
